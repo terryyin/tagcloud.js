aa789e0e8946b8466d700acf77cf416db6b0fc36
Terry Yin <terry@odd-e.com>
2014-01-10 22:00:49 +0800

    font size

--- spec/specTagCloud.js
+++ spec/specTagCloud.js
@@ -1,13 +1,42 @@
 describe("TagCloud", function() {
     var rootId = 'testContainer';
+    var canvasWidth = 150;
+    var canvasHeight = 100;
+    var container;
+    var ctx;
+    var tagCloud;
     
     beforeEach(function() {
-        var container = document.createElement('canvas');
+        container = document.createElement('canvas');
         container.setAttribute('id', rootId);
-        container.setAttribute('width', 150);
-        container.setAttribute('height', 100);
+        container.setAttribute('width', canvasWidth);
+        container.setAttribute('height', canvasHeight);
         document.body.appendChild(container);
         ctx = container.getContext("2d");
+        tagCloud = new TagCloud(canvasWidth, canvasHeight, ctx);
+    });
+
+    beforeEach(function () {
+        jasmine.addMatchers({
+            toBeFilledOverPercent: function () {
+                return {
+                     compare: function (actual, expected) {
+                            var ctx = actual;
+                            var pix = ctx.getImageData(0, 0, canvasWidth, canvasHeight).data;
+                            var pixCount = 0;
+                            for (var i = 0, n = pix.length; i < n; i += 4) {
+                                if (pix[i+3])
+                                    pixCount++;
+                            }
+                            var percent = pixCount * 100 / canvasWidth / canvasHeight;
+                            return {
+                                  pass: percent >= expected,
+                                  message: 'The filled percentage ' + percent + ' is not over '+expected
+                            };
+                    }
+                };
+            }
+        });
     });
 
     afterEach(function() {
@@ -16,8 +45,16 @@ describe("TagCloud", function() {
     });
 
     it("should take the whole place when only 1 tag", function() {
-        tagCloud = new TagCloud(ctx);
-        tagCloud.render([('Only', 1)]);
+        tagCloud.render([['Only', 1]]);
+        expect(ctx).toBeFilledOverPercent(3);
     });
+
+    it("should write the tag at a random place when only 1 tag", function() {
+        spyOn(ctx, 'fillText').returnValue = 3;
+        //spyOn(tagCloud, 'randomX');
+        tagCloud.render([['Only', 1]]);
+        expect(ctx.fillText).toHaveBeenCalledWith('Only', jasmine.any(Number), jasmine.any(Number));
+    });
+
 });
 
--- tagcloud.js
+++ tagcloud.js
@@ -1,11 +1,15 @@
-function TagCloud(context) {
+function TagCloud(w, h, context) {
     ctx = context;
+    width = w;
+    height = h;
 }
 
 TagCloud.prototype.render = function(tags) {
     var tags = tags;
-
+    var fontSize = height / 5;
+    ctx.font = "" + fontSize + "pt Arial";
+    ctx.textBaseline = "top";
     ctx.fillStyle = "rgb(0,0,0)";
-    ctx.fillText(tags[0][0], 25, 25);
+    ctx.fillText(tags[0][0], 0, 0);
 }
 
