78ef871083a4eb40a714bfc5adc4e78eaf98af93
Terry Yin <terry@odd-e.com>
2014-01-11 17:09:40 +0800

    almost working

--- spec/specTagCloud.js
+++ spec/specTagCloud.js
@@ -1,7 +1,7 @@
 describe("TagCloud", function() {
     var rootId = 'testContainer';
-    var canvasWidth = 150;
-    var canvasHeight = 100;
+    var canvasWidth = 550;
+    var canvasHeight = 400;
     var container;
     var ctx;
     var tagCloud;
@@ -39,6 +39,39 @@ describe("TagCloud", function() {
         });
     });
 
+    beforeEach(function () {
+        jasmine.addMatchers({
+            toHaveNoPixOnBorder: function () {
+                return {
+                     compare: function (actual, expected) {
+                            var ctx = actual;
+                            var pix = ctx.getImageData(0, 0, canvasWidth, canvasHeight).data;
+                            var pixCount = 0;
+                            var pixOnBorder = false;
+
+                            for (var i = 0; i < canvasWidth; i ++) {
+                                if (pix[i * 4 + 3])
+                                    pixOnBorder = true;
+                                if (pix[(((canvasHeight - 1) * canvasWidth) + i) * 4 + 3])
+                                    pixOnBorder = true;
+                            }
+
+                            for (var i = 0; i < canvasHeight; i ++) {
+                                if (pix[canvasWidth * i * 4 + 3])
+                                    pixOnBorder = true;
+                                if (pix[((canvasWidth * i) + canvasWidth - 1) * 4 + 3])
+                                    pixOnBorder = true;
+                            }
+
+                            return {
+                                  pass: !pixOnBorder
+                            };
+                    }
+                };
+            }
+        });
+    });
+
     afterEach(function() {
         var container = document.getElementById(rootId);
         container.parentNode.removeChild(container);
@@ -62,13 +95,23 @@ describe("TagCloud", function() {
         expect(ctx).toBeFilledOverPercent(3);
     });
 
-    it("should not draw on painted area", function() {
-        spyOn(ctx, 'fillText').and.callThrough();
+    it("should not draw on painted area on left", function() {
+        var leftFillPixes = canvasWidth/2;
         ctx.fillStyle = "rgb(0,0,0);"
-        ctx.fillRect(0, 0, 20, height);
+        ctx.fillRect(0, 0, leftFillPixes, canvasHeight);
+        spyOn(ctx, 'fillText').and.callThrough();
         spyOn(Math, 'random').and.returnValue(0);
         tagCloud.render([['Only', 1]]);
-        expect(ctx.fillText.calls.mostRecent().args[1]).toBeGreaterThan(20);
+        expect(ctx.fillText.calls.mostRecent().args[1]).not.toBeLessThan(leftFillPixes);
+    });
+
+
+    it("should use smaller font when no enough space", function() {
+        var leftFillPixes = canvasWidth - 30;
+        ctx.fillStyle = "rgb(0,0,0);"
+        ctx.fillRect(1, 1, leftFillPixes, canvasHeight-2);
+        tagCloud.render([['Only', 1]]);
+        expect(ctx).toHaveNoPixOnBorder();
     });
 
     it("should draw text when 2 tags", function() {
--- tagcloud.js
+++ tagcloud.js
@@ -1,20 +1,77 @@
 function TagCloud(w, h, context) {
     ctx = context;
-    width = w;
-    height = h;
+    canvasWidth = w;
+    canvasHeight = h;
 }
 
 TagCloud.prototype.render = function(tags) {
+    ctx.textBaseline = "top";
     for (tag in tags) {
-        var fontSize = height / 5;
-        ctx.font = "" + fontSize + "pt Arial";
-        ctx.textBaseline = "top";
-        ctx.fillStyle = "rgb(0,0,0)";
-        var lineHeight=ctx.measureText('M').width;
-        var matrics = ctx.measureText(tags[0][0]);
-        var x = (width - matrics.width) * Math.random();
-        var y = (height - lineHeight) * Math.random();
-
-        ctx.fillText(tags[tag][0], x, y);
+        this._placeTag(tags[tag][0]);
     }
+};
+
+TagCloud.prototype._placeTag = function(tag) {
+    var fontSize = canvasHeight / 4;
+    var placement;
+    while (!(placement = this._getNonOverlappingPlaceWithBestSize(fontSize, tag)))
+        fontSize *= 0.9;
+
+    ctx.fillText(tag, placement.x, placement.y);
+};
+
+TagCloud.prototype._getNonOverlappingPlaceWithBestSize = function(fontSize, tag) {
+    ctx.font = "" + fontSize + "pt Arial";
+    ctx.fillStyle = "rgb(0,0,0)";
+    var lineHeight=ctx.measureText('M').width * 1.1;
+    var matrics = ctx.measureText(tag);
+
+    var base = new BasePlacement(
+        (canvasWidth - matrics.width) * Math.random(),
+        (canvasHeight - lineHeight) * Math.random()
+        );
+
+    var placement;
+    while (placement = base.nextPlaceToTry()) {
+        if (this._isPlaceEmpty(placement, matrics.width, lineHeight))
+            break;
+    }
+    return placement;
+};
+
+TagCloud.prototype._isPlaceEmpty = function(placement, width, height) {
+    if (placement.x < 0 || placement.y < 0 || placement.x + width > canvasWidth || placement.y + height > canvasHeight)
+        return false;
+
+    var pix = ctx.getImageData(placement.x, placement.y, width, height).data;
+
+    for (var i = 0, n = pix.length; i < n; i += 4) {
+        if (pix[i+3]) {
+                return false;
+        }
+    }
+    return true;
+};
+
+function BasePlacement(x, y) {
+    var baseX = x,
+        baseY = y,
+        tryNumber = 0;
+
+    this.nextPlaceToTry = function() {
+        if (tryNumber < this._spiralOffsets.length)
+            return {
+                x : baseX + this._spiralOffsets[tryNumber][0],
+                y : baseY + this._spiralOffsets[tryNumber++][1]
+            };
+    };
 }
+
+BasePlacement.prototype._spiralOffsets = [
+    [0, 0],
+    [10, 0],
+    [-35, 0],
+    [280, 0]
+];
+
+
